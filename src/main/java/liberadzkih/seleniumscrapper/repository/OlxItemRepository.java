d9033c3dd1e4a96870c1eb7e9424db3ef2ed4bd8
Hubert Liberadzki <hubert.liberadzki@devapo.pl>
2023-11-26 01:26:57 +0100

    added caching

--- src/main/java/liberadzkih/seleniumscrapper/repository/OlxItemRepository.java
+++ src/main/java/liberadzkih/seleniumscrapper/repository/OlxItemRepository.java
@@ -1,10 +1,20 @@
 package liberadzkih.seleniumscrapper.repository;
 
+import java.util.List;
+
 import liberadzkih.seleniumscrapper.model.OlxItem;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OlxItemRepository extends JpaRepository<OlxItem, String> {
 
+    @Cacheable(cacheNames = "olx_item_find_all")
+    List<OlxItem> findAll();
+
+    @Caching(evict = {@CacheEvict(cacheNames = "olx_item_find_all", allEntries = true)})
+    OlxItem save(final OlxItem olxItem);
 }
