08d73a781eb2101d45a64c842e526f5a5f896e07
Hubert Liberadzki <hubert.liberadzki@devapo.pl>
2023-12-21 14:43:29 +0100

    fix-hiace

--- src/main/java/liberadzkih/seleniumscrapper/config/ConfigurationVariables.java
+++ src/main/java/liberadzkih/seleniumscrapper/config/ConfigurationVariables.java
@@ -18,7 +18,7 @@ public class ConfigurationVariables {
             "%5Bfilter_enum_model%5D%5B0%5D=w124&search%5Bfilter_enum_petrol%5D%5B0%5D=diesel",
         "https://www.olx.pl/motoryzacja/samochody/mercedes-benz/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:to%5D=6000&search" +
             "%5Bfilter_enum_model%5D%5B0%5D=190-seria&search%5Bfilter_enum_petrol%5D%5B0%5D=diesel",
-        "https://www.olx.pl/motoryzacja/dostawcze/q-HIACE/?search%5Bfilter_float_price%3Ato%5D=10000&search%5Border%5D=created_at%3Adesc",
+//        "https://www.olx.pl/motoryzacja/dostawcze/q-HIACE/?search%5Bfilter_float_price%3Ato%5D=10000&search%5Border%5D=created_at%3Adesc",
         "https://www.olx.pl/motoryzacja/samochody/toyota/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:to%5D=10000&search" +
             "%5Bfilter_enum_model%5D%5B0%5D=rav-4&search%5Bfilter_enum_petrol%5D%5B0%5D=diesel",
         "https://www.olx.pl/motoryzacja/samochody/toyota/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:to%5D=30000&search" +
